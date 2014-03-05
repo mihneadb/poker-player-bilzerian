b6a677b0c1471ccb3d5cb20b2b44f4be6180c6a5
devill <devillmeister@gmail.com>
2014-03-05 22:06:08 +0100

    Initial python code

---
+++ player.py
@@ -0,0 +1,8 @@
+
+class Player:
+    def betRequest(self, game_state):
+        return 453
+
+    def showdown(self, game_state):
+        pass
+
---
+++ player_service.py
@@ -0,0 +1,49 @@
+import time
+import cgi
+import json
+import BaseHTTPServer
+from player import Player
+
+
+HOST_NAME = 'localhost'
+PORT_NUMBER = 9000
+
+
+class PlayerService(BaseHTTPServer.BaseHTTPRequestHandler):
+
+    def do_POST(self):
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+
+        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+        if ctype == 'multipart/form-data':
+            postvars = cgi.parse_multipart(self.rfile, pdict)
+        elif ctype == 'application/x-www-form-urlencoded':
+            length = int(self.headers.getheader('content-length'))
+            postvars = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
+        else:
+            postvars = {}
+
+        game_state = json.loads(postvars['game_state'][0])
+        action = postvars['action'][0]
+
+        response = ''
+        if action == 'bet_request':
+            response = Player().betRequest(game_state)
+        elif action == 'showdown':
+            Player().showdown(game_state)
+
+        self.wfile.write(response)
+
+if __name__ == '__main__':
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), PlayerService)
+    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
