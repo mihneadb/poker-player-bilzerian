e04e462a621c2fad33b70c04231d1661e7b08a38
devill <devillmeister@gmail.com>
2014-03-08 16:58:45 +0100

    Missing char

--- player_service.py
+++ player_service.py
@@ -30,7 +30,7 @@ class PlayerService(BaseHTTPServer.BaseHTTPRequestHandler):
 
         if 'game_state' in postvars: 
             game_state = json.loads(postvars['game_state'][0])
-        else
+        else:
             game_state = {}
 
 
