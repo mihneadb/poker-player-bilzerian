bb164031e93aca6c20650a24a34065ea71c2b5d2
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2015-04-25 10:09:45 +0200

    Logging.

--- player.py
+++ player.py
@@ -1,3 +1,8 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
+
 
 class Player:
     VERSION = "Call me maybe"
@@ -5,6 +10,8 @@ class Player:
     def betRequest(self, game_state):
         me = game_state['in_action']
         call = game_state['current_buy_in'] - game_state['players'][me]['bet']
+
+        logger.info("Betting: " + call)
         return call
 
     def showdown(self, game_state):
