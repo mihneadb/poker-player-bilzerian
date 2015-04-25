9300fa27875b38e57ab8bd9174e2c2d90fb0f3cd
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2015-04-25 09:59:53 +0200

    Always call.

--- player.py
+++ player.py
@@ -3,7 +3,9 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        return 0
+        me = game_state['in_action']
+        call = game_state['current_buy_in'] - game_state['players'][me]['bet']
+        return call
 
     def showdown(self, game_state):
         pass
